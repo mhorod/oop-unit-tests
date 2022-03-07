de32a679eee14a960a73310d56d844dd7389a005
Vlad Kozulin <45079123+watislaf@users.noreply.github.com>
2022-03-07 06:41:34 +0100

    Merge Watislaf fixes (#5)
    * Update README.md
    
    * grube testy
    
    * małe fixy
    
    * no to po herbacie
    
    * format() removed

--- satori/A/VladKoz/VladKozPhoneBookTest.java
+++ satori/A/VladKoz/VladKozPhoneBookTest.java
@@ -5,10 +5,12 @@ import java.util.Random;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-public class VladKozPhoneBookTest {
+ 
+public class VladKozPhoneBookTest
+{
     @Test
-    public void addingElementsWithHostileIntent() {
+    public void addingElementsWithHostileIntent()
+    { 
         PhoneBook a = new PhoneBook();
         a.add("asdad");
         a.add("3 2 1");
@@ -29,8 +31,9 @@ public class VladKozPhoneBookTest {
         assertEquals("{\n  111-111-111\n}\n", a.toString());
     }
 
-    @Test
-    public void testCopy() {
+    @Test 
+    public void testCopy()
+    { 
         PhoneBook g1 = new PhoneBook().add("111000001").add("111000002").add("111000003");
         PhoneBook g2 = g1.copyBook();
         g2.changeFormat(PhoneBook.NumberFormat.HYPHENED);
@@ -43,11 +46,13 @@ public class VladKozPhoneBookTest {
         assertTrue(g1.subsetOf(g2));
     }
 
-    @Test
-    public void EmptyAdd() {
+    @Test 
+    public void EmptyAdd()
+    {
         var pb = new PhoneBook(1);
         var empty = new PhoneBook(99);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        { 
             pb.add(empty);
         }
         assertFalse(pb.isEmpty());
@@ -69,8 +74,9 @@ public class VladKozPhoneBookTest {
         assertTrue(obj.isEmpty());
     }
 
-    @Test
-    public void isEmptyEpty() {
+    @Test 
+    public void isEmptyEpty()
+    { 
         var empty100proc = new PhoneBook(0);
         assertTrue(empty100proc.isEmpty());
         assertTrue(empty100proc.isFull());
@@ -94,40 +100,49 @@ public class VladKozPhoneBookTest {
         pbEmpyADd.add("111111111");
         assertTrue(pbEmpyADd.isEmpty());
     }
-
-    private void RandomFillNum(PhoneBook b, int n, int seed) {
+ 
+    private void RandomFillNum(PhoneBook b, int n, int seed, PhoneBook.NumberFormat format)
+    {
         var rand = new Random();
         rand.setSeed(seed);
-        for (int i = 0; i < n; i++) { // SORRY MY FRIEND BUT you need to write getter on pb.format()
-            if (b.format() == PhoneBook.NumberFormat.DIGITS) {
+        for (int i = 0; i < n; i++)
+        {
+            if (format == PhoneBook.NumberFormat.DIGITS)
+            {
                 b.add(randomTrioString(rand) +
                         randomTrioString(rand) +
                         randomTrioString(rand));
-            } else {
+            } else
+            { 
                 b.add(randomTrioString(rand) + "-"
                         + randomTrioString(rand) + "-"
                         + randomTrioString(rand));
             }
         }
     }
-
-    private void RandomFillBooks(PhoneBook b, int n, int seed) {
+ 
+    private void RandomFillBooks(PhoneBook b, int n, int seed,
+                                 PhoneBook.NumberFormat format)
+    {
         var rand = new Random();
         rand.setSeed(seed);
-        for (int i = 0; i < n; i++) {
-            var tmp = new PhoneBook();
-            RandomFillNum(tmp, 2, seed + i);
+        for (int i = 0; i < n; i++)
+        {
+            var tmp = new PhoneBook(format);
+            RandomFillNum(tmp, 2, seed + i, format); 
             b.add(tmp);
         }
     }
 
-
-    private String randomTrioString(Random rand) {
+ 
+    private String randomTrioString(Random rand)
+    { 
         return ((Integer) (rand.nextInt(899) + 100)).toString();
     }
 
-    @Test
-    public void testEquals() {
+    @Test 
+    public void testEquals()
+    { 
         {
             var pb1 = new PhoneBook(0);
             var pb2 = new PhoneBook(PhoneBook.NumberFormat.HYPHENED);
@@ -135,29 +150,31 @@ public class VladKozPhoneBookTest {
         }
         {
             var pb1 = new PhoneBook(null, -4);
-            var pb2 = new PhoneBook();
-            RandomFillNum(pb1, 12, 42);
-            RandomFillNum(pb2, 12, 42);
+            var pb2 = new PhoneBook(); 
+            RandomFillNum(pb1, 12, 42, PhoneBook.NumberFormat.DIGITS);
+            RandomFillNum(pb2, 12, 42, PhoneBook.NumberFormat.DIGITS); 
             assertEquals(pb1, pb2);
             assertEquals(pb1.capacity(), pb1.size());
             assertEquals(pb1, pb2);
         }
         {
             var pb1 = new PhoneBook().add("111111111").add("222222222");
-            var pb2 = new PhoneBook().add("222222222").add("111111111");
-            RandomFillNum(pb1, 2, 42);
-            RandomFillNum(pb2, 2, 42);
+            var pb2 = new PhoneBook().add("222222222").add("111111111"); 
+            RandomFillNum(pb1, 2, 42, PhoneBook.NumberFormat.DIGITS);
+            RandomFillNum(pb2, 2, 42, PhoneBook.NumberFormat.DIGITS);
+ 
             assertEquals(pb1, pb2);
         }
         {
             var pb1 = new PhoneBook(PhoneBook.NumberFormat.HYPHENED);
             var pb2 = new PhoneBook();
-
-            RandomFillBooks(pb1, 2, 42);
+ 
+            RandomFillBooks(pb1, 2, 42, PhoneBook.NumberFormat.HYPHENED);
             var tmp = new PhoneBook();
-            RandomFillNum(tmp, 2, 42);
+            RandomFillNum(tmp, 2, 42, PhoneBook.NumberFormat.DIGITS);
             var tmp2 = new PhoneBook();
-            RandomFillNum(tmp, 2, 43);
+            RandomFillNum(tmp2, 2, 43, PhoneBook.NumberFormat.DIGITS);
+ 
             pb2.add(tmp2);
             pb2.add(tmp);
             assertEquals(pb1, pb2);
@@ -168,11 +185,13 @@ public class VladKozPhoneBookTest {
             var pb3 = new PhoneBook(PhoneBook.NumberFormat.HYPHENED);
             var pb4 = new PhoneBook(PhoneBook.NumberFormat.DIGITS);
             var pb5 = new PhoneBook(PhoneBook.NumberFormat.HYPHENED, 10);
-            RandomFillBooks(pb1, 11, 42);
-            RandomFillBooks(pb2, 11, 42);
-            RandomFillBooks(pb3, 11, 42);
-            RandomFillBooks(pb4, 11, 42);
-            RandomFillBooks(pb5, 11, 42);
+ 
+            RandomFillBooks(pb1, 11, 42, PhoneBook.NumberFormat.DIGITS);
+            RandomFillBooks(pb2, 11, 42, PhoneBook.NumberFormat.DIGITS);
+            RandomFillBooks(pb3, 11, 42, PhoneBook.NumberFormat.HYPHENED);
+            RandomFillBooks(pb4, 11, 42, PhoneBook.NumberFormat.DIGITS);
+            RandomFillBooks(pb5, 11, 42, PhoneBook.NumberFormat.HYPHENED);
+ 
             assertEquals(pb1, pb2);
             assertEquals(pb3, pb2);
             assertEquals(pb3, pb4);
@@ -182,14 +201,19 @@ public class VladKozPhoneBookTest {
         {
             var pb2 = new PhoneBook(11);
             var pb5 = new PhoneBook(PhoneBook.NumberFormat.HYPHENED, 11);
-            RandomFillBooks(pb2, 11, 42);
-            RandomFillBooks(pb5, 11, 42);
+ 
+            RandomFillBooks(pb2, 11, 42,PhoneBook.NumberFormat.DIGITS);
+            RandomFillBooks(pb5, 11, 42,PhoneBook.NumberFormat.HYPHENED);
+ 
             assertEquals(pb2, pb5);
         }
     }
 
     @Test
-    public void testToString() {
+ 
+    public void testToString()
+    {
+ 
         {
             var pbST = new PhoneBook();
             pbST.add(pbST);
@@ -217,7 +241,10 @@ public class VladKozPhoneBookTest {
     }
 
     @Test
-    public void copyBook() {
+ 
+    public void copyBook()
+    {
+ 
         var pb42 = new PhoneBook().add("420000000");
         var pb43 = new PhoneBook().add("430000000");
         var pbpb43 = new PhoneBook().add(pb43);
@@ -236,21 +263,31 @@ public class VladKozPhoneBookTest {
     }
 
     @Test
-    public void size() {
+ 
+    public void size()
+    {
         var ofiara = new PhoneBook();
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 15; i++)
+        {
+ 
             ofiara.add(new PhoneBook());
         }
         assertEquals(0, ofiara.size());
 
-        for (int i = 0; i < 15; i++) {
+ 
+        for (int i = 0; i < 15; i++)
+        {
+ 
             ofiara.add(new PhoneBook().add("123123123"));
         }
         assertEquals(1, ofiara.size());
     }
 
     @Test
-    public void isEmpty() {
+ 
+    public void isEmpty()
+    {
+ 
         var bookE = new PhoneBook();
         var bookN = new PhoneBook().add("123123123");
         var bookPB = new PhoneBook().add(bookE);
@@ -262,7 +299,10 @@ public class VladKozPhoneBookTest {
     }
 
     @Test
-    public void elementOf() {
+ 
+    public void elementOf()
+    {
+ 
         PhoneBook g0 = new PhoneBook().add("111000001").add("111000003");
         PhoneBook g1 = new PhoneBook().add("111000001").add("111000002").add("111000003");
         PhoneBook g2 = new PhoneBook().add("222000001").add("222000002").add("222000003");
